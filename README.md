218e22f6486f05feaedfc72356e3745ebe26a220
minori-wakade <minoriwakade@gmail.com>
2026-01-02 14:21:09 +0530

    Added gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-Add test files to this drive link: https://drive.google.com/drive/folders/1UfZFYBQi3qzgTMGmoWzK3Re_8sizelz9?usp=sharing
--- utils.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-def video_to_wav(video_path, wav_path="temp.wav"):
-    clip = VideoFileClip(video_path)
-    clip.audio.write_audiofile(wav_path,fps=48000)
-    clip.close()
-    return wav_path
